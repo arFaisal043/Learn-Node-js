1b0ccc18cc36a3b9bf7c1c426959286110eea0a6
arFaisal043 <arfaisal463@gmail.com>
2026-01-22 19:15:15 +0600

    learn path module

---
+++ path-module.js
@@ -0,0 +1,25 @@
+// Path Module: for find files location
+
+const path = require("path");
+const filesDir = path.join(__dirname, "path-module");
+
+// find files directory
+console.log(filesDir); // C:\Users\USER\Desktop\Learn Node JS\path-module
+
+// find extension name of any file
+console.log(path.extname("demo.txt")); // .txt
+
+// find base name of a file
+console.log(path.basename(filesDir)); // path-module
+
+// find a path detailed object
+console.log(path.parse(filesDir)); //return a object
+/*
+{
+  root: 'C:\\',
+  dir: 'C:\\Users\\USER\\Desktop\\Learn Node JS',
+  base: 'path-module',
+  ext: '',
+  name: 'path-module'
+}
+*/
