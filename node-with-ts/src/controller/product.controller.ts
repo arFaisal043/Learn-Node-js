cacfbdd3b46bfd01daca3f69037f7bd201889142
arFaisal043 <arfaisal463@gmail.com>
2026-05-19 16:39:31 +0600

    complete a mini project using node and TS

--- node-with-ts/src/controller/product.controller.ts
+++ node-with-ts/src/controller/product.controller.ts
@@ -1,8 +1,9 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import { readProduct } from "../service/product.service";
+import { insertProduct, readProduct } from "../service/product.service";
 import { read } from "node:fs";
 import type { InProduct } from "../Type/product.type";
 import { parseBody } from "../utility/parseBody";
+import { parse } from "node:path";
 
 export const productController = async (req: IncomingMessage, res: ServerResponse) => {
   //console.log("Request", req);
@@ -15,7 +16,7 @@ export const productController = async (req: IncomingMessage, res: ServerRespons
 
   const products = readProduct();
 
-  // All products
+  // __________ All products _____________________
   if (url === "/products" && method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
@@ -24,28 +25,92 @@ export const productController = async (req: IncomingMessage, res: ServerRespons
         data: { products },
       }),
     );
-  } // single products by id
+  } // _________ single products by id ________________________
   else if (method === "GET" && id != null) {
     const products = readProduct();
     const product = products.find((val: InProduct) => val.id === id);
     // console.log(product); ==> { id: 1, name: 'Product 1' }
 
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        message: "This product route",
-        data: product,
-      }),
-    );
-  }
-  else if(method === "POST" && url === "/products") {
+    if (!product) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "This product route",
+          data: product,
+        }),
+      );
+    }
+  } // ____________ create a product  _____________________
+  else if (method === "POST" && url === "/products") {
+    const products = readProduct();
     const body = await parseBody(req);
-    console.log("Body", body);
+
+    const newProduct = { id: Date.now(), ...body };
+    products.push(newProduct);
+    insertProduct(products); // save products on db.json file
+
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
         message: "This is post request",
-        // data: product,
+        data: products,
+      }),
+    );
+  } // ___________ update product  _____________________
+  else if (method === "PUT" && id != null) {
+    const body = await parseBody(req);
+    const products = readProduct();
+
+    const idx = products.findIndex((val: InProduct) => val.id === id);
+    // console.log(idx); // products/1 => idx = 0
+    // console.log(products[idx]);
+    if (idx < 0) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "products not found",
+          data: null,
+        }),
+      );
+    }
+    products[idx] = { id: products[idx].id, ...body };
+    insertProduct(products);
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "product updated successfully",
+        data: products[idx],
+      }),
+    );
+  } // ___________ delete product  _____________________
+  else if (method === "DELETE" && id != null) {
+    const product = readProduct();
+    const idx = products.findIndex((val: InProduct) => val.id === id);
+
+    if (idx < 0) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "products not found",
+          data: null,
+        }),
+      );
+    }
+
+    // _____ remove element in an array:
+    // const arr = [1, 2, 3, 4];
+    // arr.splice(2, 1); // [1, 2, 4]
+
+    products.splice(idx, 1);
+    // console.log(products);
+    insertProduct(products);
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "product deleted successfully",
+        data: products[idx],
       }),
     );
   }
--- node-with-ts/src/server.ts
+++ node-with-ts/src/server.ts
@@ -1,10 +1,9 @@
 import { createServer, IncomingMessage, Server, ServerResponse } from "node:http";
 import { routeHandler } from "./routes/routes";
-
-const PORT = 5173;
+import config from "./config/env.config";
 
 const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
     routeHandler(req, res);
 })
 
-server.listen(PORT, () => console.log(`Server is created on http://localhost:${PORT}`));
+server.listen(config.port, () => console.log(`Server is created on http://localhost:${config.port}`));
